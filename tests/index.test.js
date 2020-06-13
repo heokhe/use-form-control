884a6ec81a8f4ec8f692bedc0f66889f8b124a08
Hosein Khansari <hrk.x11@gmail.com>
2020-06-13 17:28:00 +0430

    Add tests for additional errors

--- tests/index.test.js
+++ tests/index.test.js
@@ -5,26 +5,32 @@ import useFormControl from '../src';
 afterEach(cleanup);
 
 function TestInput({ defaultValue = '' }) {
-  const [ref, { value, error, focus }] = useFormControl(/^[a-z]+!$/i);
+  const [ref, {
+    value, error, focus, additionalError, setAdditionalError
+  }] = useFormControl(/^[a-z]+!$/i);
+  // think of this function as the result of a "server-side validation"
+  const submit = () => setAdditionalError('Invalid name');
   return <>
     <input ref={ref} className={error ? 'error' : ''} defaultValue={defaultValue} />
     <span>{value}</span>
+    <span>{additionalError}</span>
     <button onClick={focus}>Focus</button>
+    <button onClick={submit}>Submit</button>
   </>;
 }
 
 function setup(defaultValue) {
   const { container } = render(<TestInput defaultValue={defaultValue} />);
   const [input] = container.getElementsByTagName('input');
-  const [span] = container.getElementsByTagName('span');
-  const [button] = container.getElementsByTagName('button');
+  const [valueSpan, additionalErrorSpan] = container.getElementsByTagName('span');
+  const [focusButton, submitButton] = container.getElementsByTagName('button');
   return {
-    container, input, span, button
+    container, input, valueSpan, focusButton, additionalErrorSpan, submitButton
   };
 }
 
 test('does some basic validations', () => {
-  const { input, span } = setup('hi!');
+  const { input, valueSpan: span } = setup('hi!');
 
   expect(input.classList.contains('error')).toBe(false);
   expect(span.textContent).toBe('hi!');
@@ -39,14 +45,27 @@ test('does some basic validations', () => {
 });
 
 test('can focus the input', () => {
-  const { input, button } = setup();
+  const { input, focusButton: button } = setup();
   fireEvent.click(button);
   expect(document.activeElement).toBe(input);
 });
 
 test('handles blur event', () => {
-  const { input, button } = setup();
+  const { input, focusButton: button } = setup();
   fireEvent.click(button);
   fireEvent.blur(input);
   expect(input.classList.contains('error')).toBe(true);
+});
+
+test('handles additional errors', () => {
+  const { input, additionalErrorSpan, submitButton } = setup();
+  fireEvent.change(input, { target: { value: 'yo' } });
+  // The input is surely invalid at this point.
+  fireEvent.click(submitButton);
+  expect(additionalErrorSpan.textContent.length > 0).toBe(true);
+  expect(input.classList.contains('error')).toBe(true);
+  // So, Let's fix the typo:
+  fireEvent.change(input, { target: { value: `${input.value}!` } });
+  expect(additionalErrorSpan.textContent.length).toBe(0);
+  expect(input.classList.contains('error')).toBe(false);
 });
