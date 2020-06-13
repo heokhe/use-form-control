594a958b418b54cb828ec895ed050f4a32703d2b
Hosein Khansari <hrk.x11@gmail.com>
2020-06-13 14:32:35 +0430

    Write some code
    Just a copy from another project

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import { useRef, useEffect, useState } from 'react';
+
+/**
+ * @param {RegExp} regex RegExp to check whether the value is valid or not.
+ * @returns {[import('react').Ref<any>, {
+     value: string,
+     isValid: boolean,
+     error: boolean,
+     focus(): void
+ * }]}
+ */
+export default function useFormControl(regex) {
+  const ref = useRef(null);
+  const [isValid, setIsValid] = useState(false);
+  const [hasBeenFocused, setHasBeenFocused] = useState(false);
+  const [hasBeenTouched, setHasBeenTouched] = useState(false);
+  const [additionalError, setAdditionalError] = useState('');
+  const handleChange = event => {
+    if (!hasBeenTouched) setHasBeenTouched(true);
+    setIsValid(regex.test(event.target.value));
+    setAdditionalError('');
+  };
+  const handleBlur = () => {
+    setHasBeenTouched(false);
+    setHasBeenFocused(true);
+  };
+  useEffect(() => {
+    const node = ref.current;
+    if (node) {
+      node.addEventListener('blur', handleBlur);
+      node.addEventListener('change', handleChange);
+      return () => {
+        node.removeEventListener('blur', handleBlur);
+        node.removeEventListener('change', handleChange);
+      };
+    }
+    return () => {};
+  }, [ref.current]);
+  return [ref, {
+    isValid,
+    error: !!additionalError || ((hasBeenFocused || hasBeenTouched) && !isValid),
+    additionalError,
+    setAdditionalError,
+    /** @returns {string} */
+    get value() {
+      return ref.current && ref.current.value;
+    },
+    focus() {
+      if (ref.current) ref.current.focus();
+    }
+  }];
+}
