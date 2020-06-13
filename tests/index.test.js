aa7f372c73529e7f35539397cbb149da37b7beea
Hosein Khansari <hrk.x11@gmail.com>
2020-06-13 17:07:08 +0430

    Add more tests (blur event, a separate test for focus, ...)

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,20 +1,31 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 import useFormControl from '../src';
 
-function TestInput() {
+afterEach(cleanup);
+
+function TestInput({ defaultValue = '' }) {
   const [ref, { value, error, focus }] = useFormControl(/^[a-z]+!$/i);
   return <>
-    <input ref={ref} className={error ? 'error' : ''} defaultValue="hi!" />
+    <input ref={ref} className={error ? 'error' : ''} defaultValue={defaultValue} />
     <span>{value}</span>
     <button onClick={focus}>Focus</button>
   </>;
 }
 
-test('does some basic validations', () => {
-  const { container } = render(<TestInput />);
+function setup(defaultValue) {
+  const { container } = render(<TestInput defaultValue={defaultValue} />);
   const [input] = container.getElementsByTagName('input');
   const [span] = container.getElementsByTagName('span');
+  const [button] = container.getElementsByTagName('button');
+  return {
+    container, input, span, button
+  };
+}
+
+test('does some basic validations', () => {
+  const { input, span } = setup('hi!');
+
   expect(input.classList.contains('error')).toBe(false);
   expect(span.textContent).toBe('hi!');
 
@@ -25,7 +36,17 @@ test('does some basic validations', () => {
   fireEvent.change(input, { target: { value: 'foo!' } });
   expect(span.textContent).toBe('foo!');
   expect(input.classList.contains('error')).toBe(false);
+});
 
-  fireEvent.click(container.getElementsByTagName('button')[0]);
+test('can focus the input', () => {
+  const { input, button } = setup();
+  fireEvent.click(button);
   expect(document.activeElement).toBe(input);
+});
+
+test('handles blur event', () => {
+  const { input, button } = setup();
+  fireEvent.click(button);
+  fireEvent.blur(input);
+  expect(input.classList.contains('error')).toBe(true);
 });
