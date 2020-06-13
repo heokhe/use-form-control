b9e95a574c64a42d9c4d4c1e550d51a96618e1ad
Hosein Khansari <hrk.x11@gmail.com>
2020-06-13 17:27:44 +0430

    Clean the code up

--- src/index.js
+++ src/index.js
@@ -13,17 +13,22 @@ export default function useFormControl(regex) {
   const ref = useRef(null);
   const [isValid, setIsValid] = useState(false);
   const [hasBeenFocused, setHasBeenFocused] = useState(false);
-  const [hasBeenTouched, setHasBeenTouched] = useState(false);
   const [additionalError, setAdditionalError] = useState('');
+  // This lets us know the user has interacted with the input after it has mounted,
+  // Or an additional error occured.
+  const [hasBeenTouched, setHasBeenTouched] = useState(false);
+
   const handleChange = event => {
     setHasBeenTouched(true);
     setIsValid(regex.test(event.target.value));
     setAdditionalError('');
   };
+
   const handleBlur = () => {
     setHasBeenTouched(false);
     setHasBeenFocused(true);
   };
+
   // eslint-disable-next-line consistent-return
   useEffect(() => {
     const node = ref.current;
@@ -35,17 +40,13 @@ export default function useFormControl(regex) {
       node.removeEventListener('change', handleChange);
     };
   }, [ref.current]);
+
   return [ref, {
     isValid,
     error: !!additionalError || ((hasBeenFocused || hasBeenTouched) && !isValid),
     additionalError,
     setAdditionalError,
-    /** @returns {string} */
-    get value() {
-      return ref.current && ref.current.value;
-    },
-    focus() {
-      ref.current.focus();
-    }
+    value: ref.current && ref.current.value,
+    focus: () => ref.current.focus()
   }];
 }
